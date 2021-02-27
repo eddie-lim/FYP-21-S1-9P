df6dcf181af96678c027a885509e930df09ac16a
Eddie Lim <eddielimofficial@gmail.com>
2021-02-27 08:55:29 +0800

    map ok

--- mobile/src/helpers/Environment.js
+++ mobile/src/helpers/Environment.js
@@ -1,4 +1,5 @@
 export default Environment = {
     // PROD URL
     API_URL: "https://api.openhouse.com/v1",
+    MAPBOX_KEY: "pk.eyJ1IjoibG95dGhlbWFuIiwiYSI6ImNqZXRscW5teTJmNGoydnRhb3kyMHIxaDAifQ.4Rpz7HBRmteRhHAjwnSUgw",
 }
--- mobile/src/screens/ScreenMap.js
+++ mobile/src/screens/ScreenMap.js
@@ -1,22 +1,84 @@
-import React, { useEffect, useRef } from 'react';
-import { View, Dimensions, StyleSheet, ImageBackground, Text } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Dimensions, StyleSheet, ImageBackground, Text, Pressable } from 'react-native';
+import { HeaderWithBack, StyleConstant, fabStyle, ShadowStyle } from '@assets/MyStyle';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { withScreenBase, ScreenBaseType } from '@screens/withScreenBase';
+import Environment from '@helpers/Environment';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ScreenMap = (props) => {
+  const mapViewRef = useRef(null);
+  const cameraRef = useRef(null);
+  const userLocationRef = useRef(null);
+
+  const [followMap, setFollowMap] = useState(true);
+  const [hasLocationPermission, setHasLocationPermission] = useState(false);
 
   useEffect(() => {
     console.log("ScreenMap")
+    MapboxGL.setAccessToken(Environment.MAPBOX_KEY);
     props.navigation.setParams({"navOptions":{
       header: null
     }});
     return function cleanup() { } 
   }, []);
 
+  handleOnDidFinishRenderingMapFully = (e) =>{
+    console.log('handleOnDidFinishRenderingMapFully')
+    
+    cameraRef.current.setCamera({
+      centerCoordinate: [103.776027, 1.329290],
+      zoomLevel: 17,
+      animationDuration: 200,
+    });
+  }
+
+  handleOnUserLocationUpdate = (e) =>{
+    console.log('handleOnUserLocationUpdate')
+  }
+
+  handleOnPressMyLocation = (e) =>{
+    console.log('handleOnPressMyLocation')
+    cameraRef.current.setCamera({
+      centerCoordinate: [103.776027, 1.329290],
+      zoomLevel: 17,
+      animationDuration: 200,
+    });
+  }
+
+  renderUserLocation = () => {
+      if (hasLocationPermission) {
+          // if isRecording, will show an image at the latest lnglat of the polyline
+          return (
+              <MapboxGL.UserLocation ref={userLocationRef} visible={true} showsUserHeadingIndicator={true} onUpdate={throttle(10000, true, handleOnUserLocationUpdate)} />
+          )
+      }
+  }
+
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
-      <View style={{flex : 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{color:"black"}}>map</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <MapboxGL.MapView ref={mapViewRef} style={styles.map} onDidFinishRenderingMapFully={handleOnDidFinishRenderingMapFully} onTouchEnd={() => { setFollowMap(false) }}>
+            {/* removing this followUserLocation={followMap}  */}
+            <MapboxGL.Camera ref={cameraRef} zoomLevel={18} minZoomLevel={9} maxZoomLevel={19} />
+
+            {/* {renderUserLocation()} */}
+
+            <MapboxGL.UserLocation ref={userLocationRef} visible={true} showsUserHeadingIndicator={true} onUpdate={handleOnUserLocationUpdate} />
+            
+            {/* user location */}
+            {/* <MapboxGL.ShapeSource id="user-location-source" shape={userLocationGeoJson.current}>
+                <MapboxGL.SymbolLayer id="user-location-icon-layer" style={userLocationIcon} />
+            </MapboxGL.ShapeSource> */}
+
+        </MapboxGL.MapView>
+
+        <View style={{width: '100%', position: 'absolute', bottom: 0}}>
+          <Pressable onPress={handleOnPressMyLocation} style={styles.locationBtn}>
+            <Icon name={'school'} size={25} style={Platform.OS == 'android' || {width: 25, height: 25}} color={"white"}/>
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -31,5 +93,10 @@ const styles = StyleSheet.create({
   topHolder: {flexDirection: 'row', position: 'absolute', right: 10, top: 10},
   logoHolder: {width: '90%', alignSelf: 'center', alignItems: 'center', justifyContent: 'center'},
   logo: {width: (Dimensions.get('window').width) * 0.8, height: ((Dimensions.get('window').width) * 0.8)/3},
-  centerContent: {width: '100%', height: (Dimensions.get('window').height) * 0.55, justifyContent: 'space-between'}
+  centerContent: {width: '100%', height: (Dimensions.get('window').height) * 0.55, justifyContent: 'space-between'},
+  container: { flex: 1 },
+  background: { backgroundColor: "black" },
+  locationBtn: {width: 40, height: 40, position:'absolute', alignItems:'center', justifyContent: 'center', top: -45, right: 10, backgroundColor: StyleConstant.primaryColor, borderRadius: 20, ...ShadowStyle},
+  map: { flex: 1 },
+  centerView: { width: 10, backgroundColor: "transparent", position: 'absolute', top: "50%", left: "50%", zIndex: 100 },
 });
