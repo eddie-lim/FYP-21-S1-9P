f35d69c8aa151ce1a87ccb52646c6875f893671e
Eddie Lim <eddielimofficial@gmail.com>
2021-02-28 22:11:47 +0800

    meeting 7

--- microsite/index.php
+++ microsite/index.php
@@ -7,7 +7,7 @@
 	</div>
 	<div class="body-contanier">
 		<div class="tab-container row">	
-			<ul class="nav nav-pills flex-column col-2" id="myTab" role="tablist">
+			<ul class="nav nav-tabs" id="myTab" role="tablist">
 				<li class="nav-item">
 					<a class="nav-link active" id="minutes-tab" data-toggle="tab" href="#minutes" role="tab" aria-controls="minutes" aria-selected="true">Meeting Minutes</a>
 				</li>
@@ -15,14 +15,17 @@
 					<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
 				</li>
 			</ul>
-			<div class="tab-content col-10" id="myTabContent">
+			<div class="tab-content col-12 mt-3" id="myTabContent">
 				<div class="tab-pane fade show active" id="minutes" role="tabpanel" aria-labelledby="minutes-tab">
-					<ul class="nav nav-tabs" id="myTab" role="tablist">
+					<ul class="nav nav-pills flex-column col-2 d-inline-block" style="vertical-align:top;" id="myTab" role="tablist">
 						<!-- <li class="nav-item">
-							<a class="nav-link active" id="meeting7-tab" data-toggle="tab" href="#meeting7" role="tab" aria-controls="meeting7" aria-selected="false">Meeting 7</a>
+							<a class="nav-link active" id="meeting8-tab" data-toggle="tab" href="#meeting8" role="tab" aria-controls="meeting8" aria-selected="false">Meeting 8</a>
 						</li> -->
 						<li class="nav-item">
-							<a class="nav-link active" id="meeting6-tab" data-toggle="tab" href="#meeting6" role="tab" aria-controls="meeting6" aria-selected="false">Meeting 6</a>
+							<a class="nav-link active" id="meeting7-tab" data-toggle="tab" href="#meeting7" role="tab" aria-controls="meeting7" aria-selected="false">Meeting 7</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" id="meeting6-tab" data-toggle="tab" href="#meeting6" role="tab" aria-controls="meeting6" aria-selected="false">Meeting 6</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" id="meeting5-tab" data-toggle="tab" href="#meeting5" role="tab" aria-controls="meeting5" aria-selected="false">Meeting 5</a>
@@ -40,11 +43,14 @@
 							<a class="nav-link" id="meeting1-tab" data-toggle="tab" href="#meeting1" role="tab" aria-controls="meeting1" aria-selected="true">Meeting 1</a>
 						</li>
 					</ul>
-					<div class="tab-content" id="myTabContent">
-						<!-- <div class="tab-pane fade show active" id="meeting7" role="tabpanel" aria-labelledby="meeting7-tab">
-							<?php include_once("./meetings/meeting7.php") ?>
+					<div class="tab-content col-9 d-inline-block" id="myTabContent">
+						<!-- <div class="tab-pane fade show active" id="meeting8" role="tabpanel" aria-labelledby="meeting8-tab">
+							<?php include_once("./meetings/meeting8.php") ?>
 						</div> -->
-						<div class="tab-pane fade show active" id="meeting6" role="tabpanel" aria-labelledby="meeting6-tab">
+						<div class="tab-pane fade show active" id="meeting7" role="tabpanel" aria-labelledby="meeting7-tab">
+							<?php include_once("./meetings/meeting7.php") ?>
+						</div>
+						<div class="tab-pane fade" id="meeting6" role="tabpanel" aria-labelledby="meeting6-tab">
 							<?php include_once("./meetings/meeting6.php") ?>
 						</div>
 						<div class="tab-pane fade" id="meeting5" role="tabpanel" aria-labelledby="meeting5-tab">
@@ -64,6 +70,7 @@
 						</div>
 					</div>
 				</div>
+
 				<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
 					<?php include_once("_profile.php") ?>
 				</div>
---
+++ microsite/meetings/meeting7.php
@@ -0,0 +1,17 @@
+<div class="minutes-header">Meeting 7 (with internal team members)</div>
+<div class="minutes-subheader">Time:</div>
+<ul>
+	<li>26th Feb 20:45 - 21:05</li>
+</ul>
+<div class="minutes-subheader">Attendance:</div>
+<?php include("attendees.php") ?>
+<div class="minutes-subheader">Discussed Topics:</div>
+<ul>
+	<li>Discussed and finished up the Technical Design Manual</li>
+	<li>Demo progress of prototype and what components to look out for</li>
+</ul>
+
+<div class="minutes-subheader">Next Action Items - Project Team:</div>
+<ul>
+	<li>Submit Technical Design Manual to Mr Prem on 28th Feb</li>
+</ul>
