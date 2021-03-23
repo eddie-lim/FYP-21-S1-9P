3c3a745b9ab1e85d7e4fb73bf31e1011b897e2fc
Eddie Lim <eddielimofficial@gmail.com>
2021-03-23 16:56:31 +0800

    ScreenEventDetail ok

--- mobile/src/screens/ScreenEventDetail.js
+++ mobile/src/screens/ScreenEventDetail.js
@@ -1,12 +1,43 @@
-import React, { useEffect, useRef } from 'react';
-import { View, Dimensions, StyleSheet, ImageBackground, Text } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Dimensions, StyleSheet, ImageBackground, Text, ScrollView } from 'react-native';
 import { HeaderWithBack, StyleConstant, fabStyle, ShadowStyle } from '@assets/MyStyle';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { withScreenBase, ScreenBaseType } from '@screens/withScreenBase';
 import {useNavigation, useNavigationParam} from 'react-navigation-hooks';
+import Accordion from 'react-native-collapsible/Accordion';
+import * as Animatable from 'react-native-animatable';
 
 const ScreenEventDetail = (props) => {
   const { navigate, goBack } = useNavigation();
+  const [ activeSections, setActiveSections ] = useState([0]);
+
+  const SECTIONS = [
+    {
+      // title: 'title',
+      header: 'Name',
+      content: 'Developed and awarded by RMIT University, Australia. \n\nFounded in 1887, RMIT is one of Australia’s original educational institutions. \n\nRMIT has a 5-Star QS ranking for excellence in higher education and is 15th highest ranked university in Australia (QS World University Rankings 2020-21). RMIT is ranked 18th in the world and 5th in Australia among universities less than 50 years old (QS Top 50 Under 50 index 2021). \n\nRMIT University has been providing education in aerospace engineering and aviation for over 75 years. The programme offered in Singapore enables students to access the same content and quality of instruction they would receive in Melbourne. \n\nThe successful completion of the programme will provide you with a broad, coherent body of knowledge and theory related to the global aviation industry.',
+    },
+    {
+      // title: 'title',
+      header: 'Description',
+      content: 'Jul 2021 Intake\n\nLocal Applicant: 20 Jan 2021 to 30 Apr 2021',
+    },
+    {
+      // title: 'title',
+      header: 'Venue',
+      content: 'Intake Month(s)\n\nPart-time: Jan, Jul\n\nNext intake: July 2021\n\nDirect Entry (14 modules):\n\n2.5 years : 01-Jul-21 – 31-Dec-23\n\nNon-direct Entry (18 modules – Require Prep modules):\n\n3 years : 01-Jul-21 – 30-Jun-24',
+    },
+    {
+      // title: 'title',
+      header: 'Time',
+      content: 'SIM Preparatory modules:\n\nS$1,070.00 for 2 modules\n\nRMIT Preparatory modules:\n\nS$5,035.42 for 2 modules\n\nRMIT Undergraduate programme: \n\nS$35,247.94 for 14 modules, payable over 5 semesters\n\nTotal course fee: S$41,353.36 (with 7% GST)\n\nFees quoted are valid for July 2021 intake only.\n\nAll fees inclusive of prevailing GST',
+    },
+    {
+      // title: 'title',
+      header: 'School',
+      content: 'SIM Preparatory modules:\n\nS$1,070.00 for 2 modules\n\nRMIT Preparatory modules:\n\nS$5,035.42 for 2 modules\n\nRMIT Undergraduate programme: \n\nS$35,247.94 for 14 modules, payable over 5 semesters\n\nTotal course fee: S$41,353.36 (with 7% GST)\n\nFees quoted are valid for July 2021 intake only.\n\nAll fees inclusive of prevailing GST',
+    },
+  ];
 
   useEffect(() => {
     console.log("ScreenEventDetail")
@@ -16,12 +47,61 @@ const ScreenEventDetail = (props) => {
     return function cleanup() { } 
   }, []);
 
+  renderSectionTitle = (section) => {
+    return (
+      <View style={styles.content}>
+        <Text>{section.title}</Text>
+      </View>
+    );
+  };
+
+  renderHeader = (section, _, isActive) => {
+    return (
+      <Animatable.View
+        duration={300}
+        transition="backgroundColor"
+        style={[styles.header, isActive ? styles.active : styles.inactive]}>
+        <Text style={styles.headerText}>{section.header}</Text>
+      </Animatable.View>
+    );
+  };
+
+  renderContent = (section, _, isActive) => {
+    return (
+      <Animatable.View
+        duration={300}
+        transition="backgroundColor"
+        style={[styles.content, isActive ? styles.active : styles.inactive]}>
+        <Animatable.Text
+          duration={500}
+          easing="ease-out"
+          animation={isActive ? 'bounceIn' : false}>
+          {section.content}
+        </Animatable.Text>
+      </Animatable.View>
+    );
+  };
+
+  updateSections = (activeSections) => {
+    console.log("activeSections", activeSections)
+    setActiveSections(activeSections);
+  };
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
-      <View style={{flex : 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{color:"black"}}>event detail</Text>
-        <Text style={{color:"black"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut aliquam massa. Vivamus posuere tellus augue, ut condimentum sapien semper a. Vivamus cursus vehicula leo, et posuere nunc aliquam at. Maecenas convallis est sed arcu rhoncus, sed finibus dolor vestibulum. Praesent porta varius molestie. In quis quam faucibus nisi aliquam sodales at dapibus dui. Mauris pulvinar quis sem at pellentesque. Donec molestie erat vitae nisl dictum ornare.</Text>
-      </View>
+      <ScrollView style={{backgroundColor: 'rgba(245,252,255,1)'}}>
+        <View style={{flex : 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <Accordion
+          containerStyle={{width:'100%'}}
+          sections={SECTIONS}
+          activeSections={activeSections}
+          // renderSectionTitle={renderSectionTitle}
+          renderHeader={renderHeader}
+          renderContent={renderContent}
+          onChange={updateSections}
+        />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -29,11 +109,61 @@ const ScreenEventDetail = (props) => {
 export default withScreenBase(ScreenEventDetail, ScreenBaseType.MAIN);
 
 const styles = StyleSheet.create({
-  viewHolder: { flex: 1, alignItems: 'stretch', flexDirection: 'column', backgroundColor: '#ffffff' },
-  seperator: {width: '90%', height: 1.5, backgroundColor: 'gray', alignSelf: 'center', marginTop: 30},
-  imgBg: {width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'center'},
-  topHolder: {flexDirection: 'row', position: 'absolute', right: 10, top: 10},
-  logoHolder: {width: '90%', alignSelf: 'center', alignItems: 'center', justifyContent: 'center'},
-  logo: {width: (Dimensions.get('window').width) * 0.8, height: ((Dimensions.get('window').width) * 0.8)/3},
-  centerContent: {width: '100%', height: (Dimensions.get('window').height) * 0.55, justifyContent: 'space-between'}
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    paddingTop: Constants.statusBarHeight,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '300',
+    marginBottom: 20,
+  },
+  header: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  content: {
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  active: {
+    backgroundColor: 'rgba(255,255,255,1)',
+  },
+  inactive: {
+    backgroundColor: 'rgba(245,252,255,1)',
+  },
+  selectors: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  selector: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  activeSelector: {
+    fontWeight: 'bold',
+  },
+  selectTitle: {
+    fontSize: 14,
+    fontWeight: '500',
+    padding: 10,
+  },
+  multipleToggle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30,
+    alignItems: 'center',
+  },
+  multipleToggle__title: {
+    fontSize: 16,
+    marginRight: 8,
+  },
 });
