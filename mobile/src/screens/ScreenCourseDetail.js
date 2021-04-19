2ef3379008bdd026a95ec57f4377da7785e0894b
Eddie Lim <eddielimofficial@gmail.com>
2021-04-19 17:59:50 +0800

    rearranging course detail page WIP!!!

--- mobile/src/screens/ScreenCourseDetail.js
+++ mobile/src/screens/ScreenCourseDetail.js
@@ -5,6 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { withScreenBase, ScreenBaseType } from '@screens/withScreenBase';
 import {useNavigation, useNavigationParam} from 'react-navigation-hooks';
 import Accordion from 'react-native-collapsible/Accordion';
+import Collapsible from 'react-native-collapsible';
 import * as Animatable from 'react-native-animatable';
 import { capitalize, join, split, parseInt, map } from 'lodash';
 
@@ -13,7 +14,39 @@ const ScreenCourseDetail = (props) => {
   const [ activeSections, setActiveSections ] = useState([0]);
   const [ section, setSection ] = useState([]);
   const item = useNavigationParam('item');
+  /*
+  id
+  name
+  university
+  mode_of_study
+  disciplines
+  sub_disciplines
+  academic_level
+  introduction
+  programme_structure
+  admission_criteria
+  entry_qualification
+  fees
+  exemptions
+  profiles
+  assessments_exams
+  */
   const source = useNavigationParam('source');
+
+  const [ nameIsCollapsible, setNameIsCollapsible ] = useState(false);
+  const [ universityIsCollapsible, setUniversityIsCollapsible ] = useState(false);
+  const [ modeOfStudyIsCollapsible, setModeOfStudyIsCollapsible ] = useState(false);
+  const [ disciplinesIsCollapsible, setDisciplinesIsCollapsible ] = useState(false);
+  const [ subDisciplinesIsCollapsible, setSubDisciplinesIsCollapsible ] = useState(false);
+  const [ academicLevelIsCollapsible, setAcademicLevelIsCollapsible ] = useState(false);
+  const [ introductionIsCollapsible, setIntroductionIsCollapsible ] = useState(false);
+  const [ programmeStructureIsCollapsible, setProgrammeStructureIsCollapsible ] = useState(false);
+  const [ admissionCriteriaIsCollapsible, setAdmissionCriteriaIsCollapsible ] = useState(false);
+  const [ entryQualificationIsCollapsible, setEntryQualificationIsCollapsible ] = useState(false);
+  const [ feesIsCollapsible, setFeesIsCollapsible ] = useState(false);
+  const [ exemptionsIsCollapsible, setExemptionsIsCollapsible ] = useState(false);
+  const [ profilesIsCollapsible, setProfilesIsCollapsible ] = useState(false);
+  const [ assessmentsExamsIsCollapsible, setAssessmentsExamsIsCollapsible ] = useState(false);
 
   useEffect(() => {
     props.navigation.setParams({"navOptions":{
@@ -45,7 +78,7 @@ const ScreenCourseDetail = (props) => {
    BackHandler.removeEventListener('hardwareBackPress', handleBackHandler);
    navigate(source);
    return true;
- }
+  }
   
   renderSectionTitle = (section) => {
     return (
@@ -90,15 +123,78 @@ const ScreenCourseDetail = (props) => {
     <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
       <ScrollView style={{backgroundColor: 'rgba(245,252,255,1)'}}>
         <View style={{flex : 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <Accordion
-          containerStyle={{width:'100%'}}
-          sections={section}
-          activeSections={activeSections}
-          // renderSectionTitle={renderSectionTitle}
-          renderHeader={renderHeader}
-          renderContent={renderContent}
-          onChange={updateSections}
-        />
+          {/* <Accordion
+            containerStyle={{width:'100%'}}
+            sections={section}
+            activeSections={activeSections}
+            // renderSectionTitle={renderSectionTitle}
+            renderHeader={renderHeader}
+            renderContent={renderContent}
+            onChange={updateSections}
+          /> */}
+          <Text>{item.name}</Text>
+          <Text>Awarded by: {item.university}</Text>
+          <Collapsible collapsed={modeOfStudyIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.mode_of_study}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={disciplinesIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.disciplines}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={subDisciplinesIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.sub_disciplines}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={academicLevelIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.academic_level}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={introductionIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.introduction}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={programmeStructureIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.programme_structure}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={admissionCriteriaIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.admission_criteria}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={entryQualificationIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.entry_qualification}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={feesIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.fees}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={exemptionsIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.exemptions}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={profilesIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.profiles}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+          <Collapsible collapsed={assessmentsExamsIsCollapsible}>
+            <Animatable.View>
+              <Animatable.Text>{item.assessments_exams}</Animatable.Text>
+            </Animatable.View>
+          </Collapsible>
+
         </View>
       </ScrollView>
     </SafeAreaView>
