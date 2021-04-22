5812b5c8971ab459416838bbbde340f6c31c1a12
Eddie Lim <eddielimofficial@gmail.com>
2021-04-22 17:55:45 +0800

    add icon to document.php

--- web/site/frontend/views/site/documents.php
+++ web/site/frontend/views/site/documents.php
@@ -7,24 +7,24 @@ $this->title = Yii::$app->name;
 ?>
 <div class="site-index w-100">
     <div class="">
-    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/project_requirement_document.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?> Project Requirement Document
+    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/project_requirement_document.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?>&nbsp;<i class="fas fa-question-circle"></i>&nbsp;Project Requirement Document
     </div>
     <div class="">
-    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/user_requirement_document.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?> User Requirement Document
+    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/user_requirement_document.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?>&nbsp;<i class="fas fa-users"></i>&nbsp;User Requirement Document
     </div>
     <div class="">
-    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/technical_design_manual.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?> Technical Design Manual
+    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/technical_design_manual.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?>&nbsp;<i class="fas fa-cloud"></i>&nbsp;Technical Design Manual
     </div>
     <div class="">
-    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/preliminary_user_manual.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?> Preliminary User Manual
+    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/preliminary_user_manual.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?>&nbsp;<i class="fas fa-book"></i>&nbsp;Preliminary User Manual
     </div>
     <div class="">
-    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/project_prototype_slides.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?> Project Prototype Slides
+    	<?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/project_prototype_slides.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?>&nbsp;<i class="fas fa-file-powerpoint"></i>&nbsp;Project Prototype Slides
     </div>
     <div class="">
-        <?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/project_progress_report.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?> Project Progress Report
+        <?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/project_progress_report.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?>&nbsp;<i class="fas fa-tasks"></i>&nbsp;Project Progress Report
     </div>
     <div class="">
-        <?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/test_plan.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?> Test Plan
+        <?php echo Html::a('<span class="fas fa-download fa-sm"></span>', ['/doc/test_plan.pdf'], ['class' => 'btn btn-sm btn-success mt-3 mr-1', 'target'=>"_blank"]) ?>&nbsp;<i class="fas fa-check-double"></i>&nbsp;Test Plan
     </div>
 </div>
