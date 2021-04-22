de445bad0d74c1739c3a7b3727499e344a52672c
Eddie Lim <eddielimofficial@gmail.com>
2021-04-22 17:42:36 +0800

    meeting min 15

--- web/site/frontend/views/site/meeting-minutes.php
+++ web/site/frontend/views/site/meeting-minutes.php
@@ -10,10 +10,13 @@ $this->title = Yii::$app->name;
         <div class="tab-pane fade show active" id="minutes" role="tabpanel" aria-labelledby="minutes-tab">
 			<ul class="nav nav-pills flex-column col-2 d-inline-block text-center" style="vertical-align:top;" id="myTab" role="tablist">
 				<!-- <li class="nav-item">
-					<a class="nav-link active" id="meeting15-tab" data-toggle="tab" href="#meeting15" role="tab" aria-controls="meeting15" aria-selected="false">Meeting 15</a>
+					<a class="nav-link active" id="meeting16-tab" data-toggle="tab" href="#meeting16" role="tab" aria-controls="meeting16" aria-selected="false">Meeting 16</a>
 				</li> -->
 				<li class="nav-item">
-					<a class="nav-link active" id="meeting14-tab" data-toggle="tab" href="#meeting14" role="tab" aria-controls="meeting14" aria-selected="false">Meeting 14</a>
+					<a class="nav-link active" id="meeting15-tab" data-toggle="tab" href="#meeting15" role="tab" aria-controls="meeting15" aria-selected="false">Meeting 15</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" id="meeting14-tab" data-toggle="tab" href="#meeting14" role="tab" aria-controls="meeting14" aria-selected="false">Meeting 14</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" id="meeting13-tab" data-toggle="tab" href="#meeting13" role="tab" aria-controls="meeting13" aria-selected="false">Meeting 13</a>
@@ -56,10 +59,13 @@ $this->title = Yii::$app->name;
 				</li>
 			</ul>
 			<div class="tab-content col-9 d-inline-block" id="myTabContent">
-				<!-- <div class="tab-pane fade show active" id="meeting15" role="tabpanel" aria-labelledby="meeting15-tab">
-				    <?php // echo $this->render('meetings/meeting15.php') ?>
+				<!-- <div class="tab-pane fade show active" id="meeting16" role="tabpanel" aria-labelledby="meeting16-tab">
+				    <?php // echo $this->render('meetings/meeting16.php') ?>
 				</div> -->
-				<div class="tab-pane fade show active" id="meeting14" role="tabpanel" aria-labelledby="meeting14-tab">
+				<div class="tab-pane fade show active" id="meeting15" role="tabpanel" aria-labelledby="meeting15-tab">
+				    <?php echo $this->render('meetings/meeting15.php') ?>
+				</div>
+				<div class="tab-pane fade" id="meeting14" role="tabpanel" aria-labelledby="meeting14-tab">
 				    <?php echo $this->render('meetings/meeting14.php') ?>
 				</div>
 				<div class="tab-pane fade" id="meeting13" role="tabpanel" aria-labelledby="meeting13-tab">
---
+++ web/site/frontend/views/site/meetings/meeting15.php
@@ -0,0 +1,47 @@
+<div class="minutes-header">Meeting 15 (with internal team members)</div>
+<div class="minutes-subheader">Time:</div>
+<ul>
+	<li>17th Apr 15:10 - 16:15</li>
+</ul>
+<div class="minutes-subheader">Attendance:</div>
+<?php echo $this->render('./../attendees.php') ?>
+<div class="minutes-subheader">Discussed Topics:</div>
+<ul>
+	<li>Test plan document</li>
+	<li class="no-bullet">
+		<ol>
+			<li>Able to submit</li>
+		</ol>
+	</li>
+	<li>Project progress report</li>
+	<li class="no-bullet">
+		<ol>
+			<li>Screenshots outdated, would need to update.</li>
+			<li>Can consider add in our conversation with Mr. Bernard and Ms. Marisa.</li>
+		</ol>
+	</li>
+	<li>Test case document</li>
+	<li class="no-bullet">
+		<ol>
+			<li>Able to submit</li>
+		</ol>
+	</li>
+</ul>
+<div class="minutes-subheader">Next Action Items - Project Team:</div>
+<ul>
+	<li>Project progress report</li>
+	<li class="no-bullet">
+		<ol>
+			<li>Update screenshots to latest version.</li>
+			<li>Add “Handling Feedbacks & Add-On Requirements from Stakeholders”.</li>
+		</ol>
+	</li>
+	<li>Test case document</li>
+	<li class="no-bullet">
+		<ol>
+			<li>Have to finish up System Integration Test</li>
+		</ol>
+	</li>
+</ul>
+	
+	
